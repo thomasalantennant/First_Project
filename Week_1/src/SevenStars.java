399d8333a75b3682dcdf37a5b901191a3bf115a1
thomasalantennant <thomasalantennant@gmail.com>
2016-07-27 14:49:12 +0000

    Add files via upload

---
+++ Week_1/src/SevenStars.java
@@ -0,0 +1,16 @@
+
+public class SevenStars {
+
+	
+	
+	public static void main(String[] args)
+	{
+		String star = "*";
+		for (int i= 0; i < 11; i++){
+			
+			System.out.println(star);
+			star = star + "*";
+			
+		}
+	}
+}
